24dc4e752754cab6f021af45a49e6e71f286d330
Dipak 2056 <paudelsantosh508@gmail.com>
2022-05-31 11:30:12 +1000

    admin registration successfully created

---
+++ helpers/emailHelper.js
@@ -0,0 +1,31 @@
+import nodemailer from "nodemailer";
+export const sendMail = async (emailData) => {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_SMTP,
+    port: +process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Dipak ecommerce " <davon.waelchi72@ethereal.email>', // sender address
+    to: "davon.waelchi72@ethereal.email", // list of receivers
+    subject: "Please verify your email", // Subject line
+    text: `hi there please follow the link to verify your email ${emailData.url}`, // plain text body
+    html: `
+    <p>Hi, ${emailData.fName}</p>
+    <br />
+    <br />
+    <p>please follow the link provided to verify your account</p>
+    <br />
+    <br />
+    <a href=${emailData.url}>${emailData.url}</a>
+    kind regards 
+    Dipak
+
+    `,
+  });
+};
--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { encryptPassword } from "../../helpers/bcrypthelper.js";
-import { newAdminValidation } from "../middlewares/joi-validation/adminValidation.js";
-import { insertAdmin } from "../models/admin/Admin.models.js";
+import {
+  emailVerificationValidation,
+  newAdminValidation,
+} from "../middlewares/joi-validation/adminValidation.js";
+import { insertAdmin, updateAdmin } from "../models/admin/Admin.models.js";
 import { v4 as uuidv4 } from "uuid";
+import { sendMail } from "../../helpers/emailHelper.js";
 
 const router = express.Router();
 
@@ -23,16 +27,21 @@ router.post("/", newAdminValidation, async (req, res, next) => {
     const result = await insertAdmin(req.body);
     console.log(result);
 
-    result?._id
-      ? res.json({
-          status: "success",
-          message: "new admin created successfully",
-          hashPassword,
-        })
-      : res.json({
-          status: "Error",
-          message: "new admin creation caused some error",
-        });
+    if (result?._id) {
+      //create unique url and send it to the use email
+      const url = `${process.env.ROOT_URL}/admin/verify-email/?c=${result.emailValidationCode}&e=${result.email}`;
+      sendMail({ fName: result.fName, url });
+      res.json({
+        status: "success",
+        message: "new admin created successfully",
+        hashPassword,
+      });
+    } else {
+      res.json({
+        status: "Error",
+        message: "new admin creation caused some error",
+      });
+    }
   } catch (error) {
     if (error.message.includes("E11000 duplicate key")) {
       error.message = "Email already exists, please use another Email";
@@ -42,6 +51,26 @@ router.post("/", newAdminValidation, async (req, res, next) => {
     next(error);
   }
 });
+//email verification router
+router.post(
+  "/email-verification",
+  emailVerificationValidation,
+  async (req, res) => {
+    console.log(req.body);
+    const filter = req.body;
+    const update = { status: "active" };
+    const result = await updateAdmin(filter, update);
+    result?._id
+      ? res.json({
+          status: "success",
+          message: "your email successfully verified, You may Login now.",
+        })
+      : res.json({
+          status: "error",
+          message: "INvalid or expired verification link.",
+        });
+  }
+);
 
 router.patch("/", (req, res) => {
   res.json({
