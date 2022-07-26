5d9c406ab7f4672718e56a37d0605714b9d1a8b8
Dipak 2056 <paudelsantosh508@gmail.com>
2022-07-26 11:08:15 +1000

    development completed

--- src/routers/reviewRouter.js
+++ src/routers/reviewRouter.js
@@ -1,44 +1,40 @@
 import express from "express";
 const router = express.Router();
-const reviews = [
+const Review = [
   {
-    _id: "0",
-    productId: "asdf",
-    productName: "asdf",
+    _id: 1,
+    productId: "asdfa",
+    productName: "laptop",
     rating: "5",
-    reviewedBy: "krishna",
-    reviewedById: "12k",
+    reviewBy: "sam",
+    reviewedBy_id: "qwersfa2232",
   },
   {
-    _id: "1",
-    productId: "asdf",
-    productName: "asdf",
+    _id: 2,
+    productId: "a",
+    productName: "laptop asdf",
     rating: "5",
-    reviewedBy: "krishna",
-    reviewedById: "12k",
+    reviewBy: "sam",
+    reviewedBy_id: "qwersfa2232",
   },
   {
-    _id: "2",
-    productId: "asdf",
-    productName: "asdf",
+    _id: 3,
+    productId: "a",
+    productName: "laptop asfdsf",
     rating: "5",
-    reviewedBy: "krishna",
-    reviewedById: "12k",
+    reviewBy: "sam",
+    reviewedBy_id: "qwersfa2232",
   },
 ];
 
-router.get("/:_id", async (req, res, next) => {
-  try {
-    const { _id } = req.params;
-    const result = _id ? reviews.filter((item) => item._id === _id) : reviews;
-
-    res.json({
-      status: "success",
-      message: "review list",
-      result,
-    });
-  } catch (error) {
-    next(error);
-  }
+router.get("/:_id?", (req, res) => {
+  const { _id } = req.params;
+  const result = _id ? Review.filter((item) => item._id === _id) : Review;
+  res.json({
+    status: "success",
+    message: "review list",
+    result,
+  });
 });
+
 export default router;
